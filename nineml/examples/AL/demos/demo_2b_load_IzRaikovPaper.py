40f6437c055a6b33db2c888567c194254aa49da1
Michael Hull <mikehulluk@googlemail.com>
2013-10-31 16:12:34 +0000

    Adding demo script to Ivans AL component

--- nineml/examples/AL/demos/demo_2b_load_IzRaikovPaper.py
+++
@@ -1,6 +0,0 @@
-import nineml 
-
-comp = nineml.abstraction_layer.readers.XMLReader.read_component('../../../../../../catalog/sample_xml_files/IzhikevichFS_AL.xml')
-print comp
-
-
---
+++ nineml/examples/AL/demos/demo_3_load_IzRaikovPaper.py
@@ -0,0 +1,8 @@
+import nineml 
+
+iz_comp = nineml.abstraction_layer.readers.XMLReader.read_component('../../../../../../catalog/sample_xml_files/IzhikevichFS_AL.xml')
+print iz_comp
+nineml.al.writers.DotWriter.write(iz_comp, 'TestOut_Iz.dot')
+nineml.al.writers.DotWriter.build('TestOut_Iz.dot')
+
+
