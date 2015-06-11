INCF/nineml-python

added check for valid random distribution names

nineml/abstraction_layer/dynamics/syntactic_sugar.py
@@ -2,6 +2,7 @@ from nineml.utils import filter_discrete_types
 from nineml.exceptions import NineMLRuntimeError
 from .transitions import (OutputEvent, Trigger, StateAssignment, OnEvent,
                           OnCondition)
+from nineml.abstraction_layer.expressions import RandomVariable
 from nineml.abstraction_layer.expressions.utils import is_single_symbol
 
 
@@ -21,14 +22,15 @@ def cond_to_obj(cond_str):
 
 def do_to_assignments_and_events(doList):
     if not doList:
-        return [], []
+        return [], [], []
     # 'doList' is a list of strings, OutputEvents, and StateAssignments.
-    do_type_list = (OutputEvent, basestring, StateAssignment)
+    do_type_list = (OutputEvent, basestring, StateAssignment, RandomVariable)
     do_types = filter_discrete_types(doList, do_type_list)
     # Convert strings to StateAssignments:
     sa_from_strs = [StateAssignment.from_str(s)
                     for s in do_types[basestring]]
-    return do_types[StateAssignment] + sa_from_strs, do_types[OutputEvent]
+    return (do_types[StateAssignment] + sa_from_strs, do_types[OutputEvent],
+            do_types[RandomVariable])
 
 
 # Forwarding Function:
@@ -54,16 +56,20 @@ def On(trigger, do=None, to=None):
 
 def DoOnEvent(input_event, do=None, to=None):
     assert isinstance(input_event, basestring)
-    assignments, output_events = do_to_assignments_and_events(do)
+    (assignments, output_events,
+     random_variables) = do_to_assignments_and_events(do)
     return OnEvent(src_port_name=input_event,
                    state_assignments=assignments,
                    output_events=output_events,
+                   random_variables=random_variables,
                    target_regime=to)
 
 
 def DoOnCondition(condition, do=None, to=None):
-    assignments, output_events = do_to_assignments_and_events(do)
+    (assignments, output_events,
+     random_variables) = do_to_assignments_and_events(do)
     return OnCondition(trigger=condition,
                        state_assignments=assignments,
                        output_events=output_events,
+                       random_variables=random_variables,
                        target_regime=to)

test/unit/nineml_test/abstraction_layer/validators/dimensionality_test.py
@@ -6,6 +6,8 @@ from nineml.abstraction_layer import (
 from nineml.abstraction_layer.ports import AnalogSendPort, AnalogReceivePort
 from nineml import units as un
 from nineml.exceptions import NineMLDimensionError
+from nineml.abstraction_layer.expressions.random import (
+    RandomVariable, RandomDistribution)
 
 
 class Dimensionality_test(unittest.TestCase):
@@ -22,8 +24,15 @@ class Dimensionality_test(unittest.TestCase):
                 Regime(
                     'dSV1/dt = -SV1 / P2',
                     'dSV2/dt = A3 / ARP2 + SV2 / P2',
-                    transitions=[On('SV1 > P3', do=[OutputEvent('emit')]),
-                                 On('spikein', do=[OutputEvent('emit')])],
+                    transitions=[
+                        On('SV1 > P3', do=[OutputEvent('emit')]),
+                        On('spikein',
+                           do=[OutputEvent('emit'),
+                               StateAssignment('SV1', 'r'),
+                               RandomVariable(
+                                   'r', units=un.mV,
+                                   distribution=RandomDistribution(
+                                       'Gamma', shape=10.0, scale=1.0))])],
                     name='R1'
                 ),
                 Regime(name='R2', transitions=On('(SV1 > C1) & (SV2 < P4)',

test/unit/nineml_test/abstraction_layer/visitors/cloner_test.py
@@ -5,11 +5,13 @@ import warnings
 import unittest
 
 from nineml.abstraction_layer import (
-    Dynamics as Dynamics, Regime, On, OutputEvent,
+    Dynamics, Regime, On, OutputEvent,
     AnalogSendPort as SendPort, AnalogReceivePort as RecvPort,
-    NamespaceAddress)
+    NamespaceAddress, StateAssignment, RandomVariable)
+from nineml.abstraction_layer.expressions.random import RandomDistribution
 from nineml.abstraction_layer.dynamics.utils.cloner import (
     DynamicsClonerPrefixNamespace)
+import nineml.units as un
 
 NSA = NamespaceAddress
 
@@ -23,7 +25,8 @@ class DynamicsClonerPrefixNamespace_test(unittest.TestCase):
             name='D',
             aliases=['D1:=dp1', 'D2 := dIn1', 'D3 := SV1'],
             regimes=[
-                Regime('dSV1/dt = -SV1/(dp2*t)', name='r1', transitions=On('input', 'SV1=SV1+1'))],
+                Regime('dSV1/dt = -SV1/(dp2*t)', name='r1',
+                       transitions=On('input', 'SV1=SV1+1'))],
             analog_ports=[RecvPort('dIn1'), SendPort('D1'), SendPort('D2')],
             parameters=['dp1', 'dp2']
         )
@@ -34,13 +37,21 @@ class DynamicsClonerPrefixNamespace_test(unittest.TestCase):
             regimes=[
                 Regime(
                     'dSV1/dt = -SV1/(cp2*t)',
-                    transitions=[On('SV1>cp1', do=[OutputEvent('emit')]),
-                                 On('spikein', do=[OutputEvent('emit')])],
+                    transitions=[
+                        On('SV1>cp1',
+                           do=[OutputEvent('emit'),
+                               StateAssignment('SV1', 'r'),
+                               RandomVariable(
+                                   'r', units=un.unitless,
+                                   distribution=RandomDistribution(
+                                       'Gamma', shape=10.0, scale=1.0))]),
+                        On('spikein', do=[OutputEvent('emit')])],
                     name='r1',
                 ),
                 Regime(name='r2', transitions=On('SV1>1', to='r1'))
             ],
-            analog_ports=[RecvPort('cIn1'), RecvPort('cIn2'), SendPort('C1'), SendPort('C2')],
+            analog_ports=[RecvPort('cIn1'), RecvPort('cIn2'), SendPort('C1'),
+                          SendPort('C2')],
             parameters=['cp1', 'cp2']
         )
 

test/unit/nineml_test/abstraction_layer/visitors/required_definitions_test.py
@@ -1,7 +1,10 @@
 import unittest
 from itertools import chain
 from nineml.abstraction_layer.dynamics import (
-    Dynamics, Regime, On, OutputEvent)
+    Dynamics, Regime, On, OutputEvent, StateAssignment)
+from nineml.abstraction_layer import RandomVariable
+from nineml.abstraction_layer.expressions.random import RandomDistribution
+import nineml.units as un
 from nineml.abstraction_layer.ports import AnalogSendPort, AnalogReceivePort
 from nineml.abstraction_layer.expressions import reserved_identifiers
 
@@ -18,7 +21,14 @@ class DynamicsRequiredDefinitions_test(unittest.TestCase):
                 Regime('dSV1/dt = -SV1 / (P2*t)',
                        'dSV2/dt = A2/t + A3/t + ARP1/t',
                        name='R1',
-                       transitions=On('input', 'SV1 = SV1 + 1'))],
+                       transitions=On(
+                           'input',
+                           do=[StateAssignment('SV1', 'SV1 + 1'),
+                               StateAssignment('SV2', 'r'),
+                               RandomVariable(
+                                   'r', units=un.unitless,
+                                   distribution=RandomDistribution(
+                                       'Gamma', shape=10.0, scale=1.0))]))],
             analog_ports=[AnalogReceivePort('ARP1'),
                           AnalogReceivePort('ARP2'),
                           AnalogSendPort('A1'),
@@ -73,6 +83,7 @@ class DynamicsRequiredDefinitions_test(unittest.TestCase):
                           .format(atom, expression, repr(required)))
 
     def test_required_definitions(self):
+
         for component_class in (self.a, self.b):
             for regime in component_class.regimes:
                 for td in regime.time_derivatives:

russellijarvis/lib9ML

nineml/abstraction/dynamics/syntactic_sugar.py
@@ -2,6 +2,7 @@ from nineml.utils import filter_discrete_types
 from nineml.exceptions import NineMLRuntimeError
 from .transitions import (OutputEvent, Trigger, StateAssignment, OnEvent,
                           OnCondition)
+from nineml.abstraction.expressions import RandomVariable
 from nineml.abstraction.expressions.utils import is_single_symbol
 
 
@@ -21,14 +22,15 @@ def cond_to_obj(cond_str):
 
 def do_to_assignments_and_events(doList):
     if not doList:
-        return [], []
+        return [], [], []
     # 'doList' is a list of strings, OutputEvents, and StateAssignments.
-    do_type_list = (OutputEvent, basestring, StateAssignment)
+    do_type_list = (OutputEvent, basestring, StateAssignment, RandomVariable)
     do_types = filter_discrete_types(doList, do_type_list)
     # Convert strings to StateAssignments:
     sa_from_strs = [StateAssignment.from_str(s)
                     for s in do_types[basestring]]
-    return do_types[StateAssignment] + sa_from_strs, do_types[OutputEvent]
+    return (do_types[StateAssignment] + sa_from_strs, do_types[OutputEvent],
+            do_types[RandomVariable])
 
 
 # Forwarding Function:
@@ -54,16 +56,20 @@ def On(trigger, do=None, to=None):
 
 def DoOnEvent(input_event, do=None, to=None):
     assert isinstance(input_event, basestring)
-    assignments, output_events = do_to_assignments_and_events(do)
+    (assignments, output_events,
+     random_variables) = do_to_assignments_and_events(do)
     return OnEvent(src_port_name=input_event,
                    state_assignments=assignments,
                    output_events=output_events,
+                   random_variables=random_variables,
                    target_regime=to)
 
 
 def DoOnCondition(condition, do=None, to=None):
-    assignments, output_events = do_to_assignments_and_events(do)
+    (assignments, output_events,
+     random_variables) = do_to_assignments_and_events(do)
     return OnCondition(trigger=condition,
                        state_assignments=assignments,
                        output_events=output_events,
+                       random_variables=random_variables,
                        target_regime=to)

test/unit/nineml_test/abstraction_layer/validators/dimensionality_test.py
@@ -6,6 +6,8 @@ from nineml.abstraction import (
 from nineml.abstraction.ports import AnalogSendPort, AnalogReceivePort
 from nineml import units as un
 from nineml.exceptions import NineMLDimensionError
+from nineml.abstraction.expressions.random import (
+    RandomVariable, RandomDistribution)
 
 
 class Dimensionality_test(unittest.TestCase):
@@ -22,8 +24,15 @@ class Dimensionality_test(unittest.TestCase):
                 Regime(
                     'dSV1/dt = -SV1 / P2',
                     'dSV2/dt = A3 / ARP2 + SV2 / P2',
-                    transitions=[On('SV1 > P3', do=[OutputEvent('emit')]),
-                                 On('spikein', do=[OutputEvent('emit')])],
+                    transitions=[
+                        On('SV1 > P3', do=[OutputEvent('emit')]),
+                        On('spikein',
+                           do=[OutputEvent('emit'),
+                               StateAssignment('SV1', 'r'),
+                               RandomVariable(
+                                   'r', units=un.mV,
+                                   distribution=RandomDistribution(
+                                       'Gamma', shape=10.0, scale=1.0))])],
                     name='R1'
                 ),
                 Regime(name='R2', transitions=On('(SV1 > C1) & (SV2 < P4)',

test/unit/nineml_test/abstraction_layer/visitors/cloner_test.py
@@ -5,11 +5,13 @@ import warnings
 import unittest
 
 from nineml.abstraction import (
-    Dynamics as Dynamics, Regime, On, OutputEvent,
+    Dynamics, Regime, On, OutputEvent,
     AnalogSendPort as SendPort, AnalogReceivePort as RecvPort,
-    NamespaceAddress)
+    NamespaceAddress, StateAssignment, RandomVariable)
+from nineml.abstraction.expressions.random import RandomDistribution
 from nineml.abstraction.dynamics.visitors.cloner import (
     DynamicsClonerPrefixNamespace)
+import nineml.units as un
 
 NSA = NamespaceAddress
 
@@ -23,7 +25,8 @@ class DynamicsClonerPrefixNamespace_test(unittest.TestCase):
             name='D',
             aliases=['D1:=dp1', 'D2 := dIn1', 'D3 := SV1'],
             regimes=[
-                Regime('dSV1/dt = -SV1/(dp2*t)', name='r1', transitions=On('input', 'SV1=SV1+1'))],
+                Regime('dSV1/dt = -SV1/(dp2*t)', name='r1',
+                       transitions=On('input', 'SV1=SV1+1'))],
             analog_ports=[RecvPort('dIn1'), SendPort('D1'), SendPort('D2')],
             parameters=['dp1', 'dp2']
         )
@@ -34,13 +37,21 @@ class DynamicsClonerPrefixNamespace_test(unittest.TestCase):
             regimes=[
                 Regime(
                     'dSV1/dt = -SV1/(cp2*t)',
-                    transitions=[On('SV1>cp1', do=[OutputEvent('emit')]),
-                                 On('spikein', do=[OutputEvent('emit')])],
+                    transitions=[
+                        On('SV1>cp1',
+                           do=[OutputEvent('emit'),
+                               StateAssignment('SV1', 'r'),
+                               RandomVariable(
+                                   'r', units=un.unitless,
+                                   distribution=RandomDistribution(
+                                       'Gamma', shape=10.0, scale=1.0))]),
+                        On('spikein', do=[OutputEvent('emit')])],
                     name='r1',
                 ),
                 Regime(name='r2', transitions=On('SV1>1', to='r1'))
             ],
-            analog_ports=[RecvPort('cIn1'), RecvPort('cIn2'), SendPort('C1'), SendPort('C2')],
+            analog_ports=[RecvPort('cIn1'), RecvPort('cIn2'), SendPort('C1'),
+                          SendPort('C2')],
             parameters=['cp1', 'cp2']
         )
 

test/unit/nineml_test/abstraction_layer/visitors/required_definitions_test.py
@@ -1,7 +1,10 @@
 import unittest
 from itertools import chain
 from nineml.abstraction.dynamics import (
-    Dynamics, Regime, On, OutputEvent)
+    Dynamics, Regime, On, OutputEvent, StateAssignment)
+from nineml.abstraction import RandomVariable
+from nineml.abstraction.expressions.random import RandomDistribution
+import nineml.units as un
 from nineml.abstraction.ports import AnalogSendPort, AnalogReceivePort
 from nineml.abstraction.expressions import reserved_identifiers
 
@@ -18,7 +21,14 @@ class DynamicsRequiredDefinitions_test(unittest.TestCase):
                 Regime('dSV1/dt = -SV1 / (P2*t)',
                        'dSV2/dt = A2/t + A3/t + ARP1/t',
                        name='R1',
-                       transitions=On('input', 'SV1 = SV1 + 1'))],
+                       transitions=On(
+                           'input',
+                           do=[StateAssignment('SV1', 'SV1 + 1'),
+                               StateAssignment('SV2', 'r'),
+                               RandomVariable(
+                                   'r', units=un.unitless,
+                                   distribution=RandomDistribution(
+                                       'Gamma', shape=10.0, scale=1.0))]))],
             analog_ports=[AnalogReceivePort('ARP1'),
                           AnalogReceivePort('ARP2'),
                           AnalogSendPort('A1'),